cf79ea52651dc349cc5773669f2ec20fa9c6c449
Marcus <mhellams@hotmail.com>
2016-05-24 00:49:57 -0400

    Add editing capibilites to cells

--- public/js/directives/tableCellDirective.js
+++ public/js/directives/tableCellDirective.js
@@ -1,7 +1,10 @@
-define(['directives/module', 'hbs!../../templates/tableCellDirective', 'lo-dash'], function(directives, tds, _) {
+define(['directives/module', 'hbs!../../templates/tableCellDirective'], function(directives, tds) {
 	'use strict';
 
 	directives.directive('tableCellDirective', tableCellDirective);
+	directives.directive('innerCellDirective', innerCellDirective);
+
+	innerCellDirective.$inject = ['$compile'];
 
 	function tableCellDirective() {
 		return {
@@ -11,9 +14,46 @@ define(['directives/module', 'hbs!../../templates/tableCellDirective', 'lo-dash'
 				column: '='
 			},
 			// templateUrl: 'templates/tableCellDirective.html',
-			'template':'<td ng-inculde="templates/tableCellDirective.html" ng-repeat="def in options.columnDefinitions">{{column[def.name]}}</td>',
+			'template': '<td ng-inculde="templates/tableCellDirective.html" ng-repeat="def in options.columnDefinitions"><inner-cell-directive column=column options="options" definition="def"></inner-cell-directive</td>',
 			link: function(s, ele, attr) {
-				console.log(s);
+				// console.log(s);
+			}
+
+		};
+	}
+
+	function innerCellDirective($compile) {
+		return {
+			restrict: 'E',
+			scope: {
+				definition: '=',
+				column: '=',
+				options: '='
+			},
+			'template': '<span>{{column[definition.name]}}</span>',
+			link: function(s, ele, attr) {
+				var oldText = ele.html();
+				console.log(oldText);
+				var input = $('<input />').attr('ng-model', s.column[s.definition.name]);
+				input = '<div class="form-group">' +
+					'<label class="sr-only" for=""></label>' +
+					'<input type="text" class="form-control" id="" ng-model="column[definition.name]">' +
+					'</div>';
+				ele.one('dblclick', swapTextForInput);
+
+				function swapTextForInput(e) {
+					$compile(input)(s, function(cloned, s) {
+						ele.html(cloned);
+					});
+
+					ele.find(':input').one('blur focusout', function(e) {
+						$compile(oldText)(s, function(cloned, s) {
+							console.log(cloned);
+							ele.html(cloned);
+						});
+
+					});
+				}
 			}
 
 		};
